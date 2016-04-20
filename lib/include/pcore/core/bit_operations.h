52a55e5d0fc6584ae3439a336ba751c42e1ec1bf
master <pswinpo@gmail.com>
2016-04-20 23:20:06 +0430

    Bitwise operations bug fixed.

--- lib/include/pcore/core/bit_operations.h
+++ lib/include/pcore/core/bit_operations.h
@@ -8,9 +8,11 @@
 #define _PCORE_BIT_OPERATIONS_H
 
 
-
-#define _PCORE_GETMASK( __pos, __size ) (((1 << (__size)) - 1) << (__pos))
-
+/* \brif Make a mak at specified position with given length.
+ * \param __pos: Start position.
+ * \param __len: Length of mask.
+ */
+#define PCORE_GET_MASK( __pos, __len ) (((1 << (__len)) - 1) << (__pos))
 
 
 /*
@@ -22,34 +24,12 @@
 
 
 /*
- *	\brif Reads specified bits from _pos with length _size
- *	\param __var: Varable that must be readed from.
- *	\pram __pos: Start position.
- *	\param __len: Length.
- */
-#define PCORE_BIT_READ( __var, __pos, __len ) \
-					(((__var) & _PCORE_GETMASK((__pos), (__len))) >> (__pos))
-
-
-/*
- *	\brif Writes given value in specified bits of given variable.
- *	\param __var: Varable that must be writed to.
- *	\pram __pos: Start position.
- *	\param __len: Length.
- *	\param __val: value that have to be writed in specified bits of __var.
- */
-#define PCORE_BIT_WRITE( __var, __pos, __len, _val ) \
-						((__var) = ((__var) & (~_PCORE_GETMASK((__pos), \
-						(__len)))) | ((__val) << (__pos)))
-
-
-/*
  *	\brif Sets specified bit.
  *	\param __var: Varable.
  *	\pram __pos: Bit position.
  */
 #define PCORE_BIT_SET( __var, __pos ) \
-						((_var) = ((__var) & ( 1 >> __pos)))
+						((__var) = ((__var) & ( 1 >> __pos)))
 
 
 /*
@@ -58,7 +38,28 @@
  *	\pram __pos: Bit position.
  */
 #define PCORE_BIT_UNSET( __var, __pos ) \
-						((_var) = ((__var) & ( 1 >> __pos)))
+						((__var) = ((__var) & ~( 1 >> __pos)))
+
+
+/*
+ *	\brif Reads specified bits from _pos with length _size
+ *	\param __var: Varable that must be readed from.
+ *	\pram __pos: Start position.
+ *	\param __len: Length.
+ */
+#define PCORE_BIT_READ( __var, __pos, __len ) \
+						( __var & PCORE_GET_MASK(__pos,__len) )
+
+/*
+ *	\brif Writes given value in specified bits of given variable.
+ *	\param __var: Varable that must be writed to.
+ *	\pram __pos: Start position.
+ *	\param __len: Length.
+ *	\param __val: value that have to be writed in specified bits of __var.
+ */
+#define PCORE_BIT_WRITE( __var, __pos, __len, __val ) \
+							( __var = ( __val << __pos  ) | \
+							(~PCORE_GET_MASK(__pos,__len) & __var) )
 
 
 #endif // _PCORE_BIT_OPERATIONS_H
